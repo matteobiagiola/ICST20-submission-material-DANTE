583a3cb696faaabacb92c46f3c08ba375798df1c
Andrea Stocco <tsigalko18@gmail.com>
2022-08-28 15:48:29 +0200

    removed generated data

--- dante/applications/dimeshift/testsuite-dimeshift/run-flakiness-checked.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# run test suite (application has to be active on http://localhost:3000)
-cp=$(mvn dependency:build-classpath | grep ".jar:")
-
-java -cp "$cp:./target/classes" main.CheckSuiteFlakiness tests.GeneratedTestSuiteChecked
